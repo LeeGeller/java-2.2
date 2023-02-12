714ab670a88e2dd2b6da4eaf6a4118053ec1cc84
Kristina Budnik <ssspers92@gmail.com>
2023-02-12 19:21:31 +0300

    repare code

--- Main-3/src/Main.java
+++ Main-3/src/Main.java
@@ -2,22 +2,17 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        int startAccount;
-        int finalAccount;
-        int transaction;
-        int bonusRub;
-        int bonus;
 
-        bonusRub = 100;
-        startAccount = 100;
-        transaction = 1200;
+        int bonusRub = 100;
+        int startAccount = 100;
+        int transaction = 1200;
 
         if (transaction > 999) {
-            bonus = transaction/100;
-            finalAccount = startAccount + transaction + bonus;
+            int bonus = transaction / 100;
+            int finalAccount = startAccount + transaction + bonus;
             System.out.println("Ваш баланс:" + finalAccount);
-        }else {
-            finalAccount = startAccount + transaction;
+        } else {
+            int finalAccount = startAccount + transaction;
             System.out.println("Ваш баланс:" + finalAccount);
         }
     }
